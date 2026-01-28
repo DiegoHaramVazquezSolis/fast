cdae178b58fe5222ab65432f6eb520d99d9b4590
DiegoHaramVazquezSolis <diego@ac1.ai>
2026-01-28 14:55:37 -0800

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 p-4">
-      <div className="max-w-6xl w-full bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
+      <div className="max-w-7xl w-full bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
         <div className="p-6">
           <h1 className="text-2xl font-bold mb-4 text-center text-gray-800 dark:text-white">Fast Facts</h1>
           <div
